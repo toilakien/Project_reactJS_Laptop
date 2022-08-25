5364ac334b04e4836b57bc215545b86add90024b
Kiên <86771355+toilakien@users.noreply.github.com>
2022-08-25 14:53:36 +0700

    update

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,52 +1,68 @@
 import React from 'react';
 import styles from './Login.module.css';
-import {useState} from 'react';
+import { Formik } from 'formik';
 function Login(){
-    const a=JSON.parse(localStorage.getItem('acc')).tk
-    const b= JSON.parse(localStorage.getItem('acc')).mk;
-    const [accTk,setAccTk]=useState(()=>{
-        return a;
-    })
-    const [accMk,setAccMk]=useState(()=>{
-        return b;
-    })
-    const [email,setEmail]=useState('');
-    const [p1,setP1]=useState('');
-   
-  
-    const handleLogin=(e)=>{
-        if(accTk===email&&accMk===p1){
-            localStorage.setItem('load',JSON.stringify(true))
-        }else{
-            e.preventDefault();
-            alert('Thông tin tài khoản không chính xác !')
-        }
-    }
+    
     return (
         <div className={`container-fluid ${styles.loginFluid}`}>
-            <form action="/" onSubmit={handleLogin} className={styles.login}>
-                <div className="row">
-                <h1 className="text-center">Đăng nhập</h1>
-                <div className="mb-3">
+            <Formik
+
+                initialValues={{
+                    email:'',
+                    password:'',
+                    remember_me:''
+                }}
+                validate={values => {
+                    const errors = {};
+                    if (!values.email) {
+                      errors.email = 'Required';
+                    } else if (
+                      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                    ) {
+                      errors.email = 'Invalid email address';
+                    }
+                    return errors;
+                  }}
+                onSubmit={
+                    values=>{}
+                }
+            >
+                {
+                    props=>{
+                        const {values,
+                            errors,
+                            touched,
+                            handleChange,
+                            handleBlur,
+                            handleSubmit,
+                            isSubmitting}=props;
+                        
+                        return (
+                            <form onSubmit={handleSubmit}  className={styles.login}>
+                    <div className="row">
+                    <h1 className="text-center">Đăng nhập</h1>
+                    <div className="mb-3">
                     <label htmlFor='email' className="form-label">Email address</label>
-                    <input type="email"
-                    value={email}
+                    <input 
                      className="form-control" 
                      name='email'
                       placeholder="name@example.com"
-                      onChange={e=>setEmail(e.target.value)}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
                       />
+                      <div style={{color:"red"}}>{errors.email}</div>
                 </div>
                 <div className="mb-3">
-                    <label htmlFor='p1' 
+                    <label htmlFor='password' 
                     className="form-label">Password</label>
                     <input type="text" 
                     className="form-control"
-                    value={p1}
-                     name='p1'
+                     name='password'
                       placeholder="VD:Kienkekek13@"
-                      onChange={e=>setP1(e.target.value)}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
                       />
+                       <div style={{color:"red"}}>{errors.password}</div>
                 </div>
                 <div className="f-ck form-check">
                     <input className="CK form-check-input" type="checkbox" value="" id="invalidCheck2" required/>
@@ -59,6 +75,10 @@ function Login(){
                 </div>
                 </div>
             </form>
+                        )
+                    }
+                }
+            </Formik>
         </div>
     )
 }
